6a3106c2824a8aa56b5f3aa63bb9f404edf9ac21
dogura <daiki.ogura@soliton.co.jp>
2014-01-31 19:05:37 +0900

    add resource of webfont

--- static/main.js
+++ static/main.js
@@ -13,10 +13,25 @@
   });
 
   myapp.controller('EntryController', [
-    '$scope', function($scope) {
+    '$scope', '$http', function($scope, $http) {
       $scope.entry_id = '';
+      $scope.build_result = '';
       $scope.chose_id = function(entry_id) {
         return $scope.entry_id = entry_id;
+      };
+      $scope.do_build = function() {
+        if ($scope.entry_id === '') {
+          return;
+        }
+        return $http({
+          method: 'GET',
+          url: '/build/' + $scope.entry_id,
+          params: {}
+        }).success(function(data, status, headers, config) {
+          return $scope.build_result = data;
+        }).error(function(data, status, headers, config) {
+          return $scope.build_result = data;
+        });
       };
       return END;
     }
